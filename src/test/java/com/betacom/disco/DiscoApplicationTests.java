2f161b2de59842b6ca99eb9c9564eac19553cbd7
MarcoPanero <marco.panero2000@gmail.com>
2026-06-19 12:09:37 +0200

    first commit

---
+++ src/test/java/com/betacom/disco/DiscoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betacom.disco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
